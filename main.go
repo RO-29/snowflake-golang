4ab18351b16a83fbedbd3ed57f7e88b6a65284df
Rohit Jain <rohit@sendinblue.com>
2019-07-07 16:20:28 +0530

    add Init and compute custom epoch

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"github.com/RO-29/snowflake-Golang/snowflake"
+)
 
+func init() {
+	snowflake.Init()
 }
+
+func main() {}
--- snowflake/snowflake.go
+++ snowflake/snowflake.go
@@ -1,6 +1,43 @@
 package snowflake
 
-import "github.com/pkg/errors"
+import (
+	"math"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	nodeID int
+
+	// Custom Epoch (January 1, 2019 Midnight UTC = 2019-01-01T00:00:00Z)
+	customEPOCH int64
+	timeCustom  = "2019-01-01T00:00:00+00:00"
+
+	totalBITS    = int(64)
+	epochBITS    = int(42)
+	nodeIDBITS   = int(10)
+	sequenceBITS = int(12)
+	maxNodeID    = (int)(math.Pow(2, float64(nodeIDBITS)) - 1)
+	maxSequence  = (int)(math.Pow(2, float64(sequenceBITS)) - 1)
+
+	// private volatile long lastTimestamp = -1L;
+	// private volatile long sequence = 0L;
+)
+
+//Init the custom epoch
+func Init() {
+	timeMustParse()
+
+}
+
+func timeMustParse() {
+	timeObj, err := time.Parse(time.RFC3339, timeCustom)
+	if err != nil {
+		panic(err)
+	}
+	customEPOCH = timeObj.Unix()
+}
 
 //Snowflake service ...
 type Snowflake struct{}
